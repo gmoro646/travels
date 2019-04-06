59bc3fcc58f36c625e9d6672b38f55202261f529
vishal1145 <vishal.kumar1145@gmail.com>
2019-04-06 18:47:35 +0530

    login api added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,6 +4,53 @@ import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
 class Home extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            uploadInProgress: false,
+            user: {},
+        };
+    }
+
+    updatePropertyValue(val, prop) {
+        let usr = this.state.user || {};
+        usr[prop] = val;
+        this.setState({ user: usr });
+    }
+
+    async executeAPI() {
+        return new Promise((resolve, reject) =>{
+            var data = JSON.stringify({
+                "username": this.state.user.email,
+                "password": this.state.user.password
+            });
+
+            var xhr = new XMLHttpRequest();
+            //xhr.withCredentials = true;
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === 4) {
+                    resolve(this.responseText)
+                }
+            });
+
+            xhr.open("POST", "http://localhost:8080/login");//need to replace with base url from some config
+            xhr.setRequestHeader("content-type", "application/json");
+
+            xhr.send(data);
+        })
+    }
+    async login() {
+        console.log(this.state)
+        let response = await this.executeAPI();
+        console.log(response);
+        var userdata = JSON.parse(response)
+        localStorage.setItem('token', userdata.token)
+        //browserHistory.push('/home');
+        //this.props.history.push('/home')
+        window.location.href = '/home'
+    }
+
 	render() {
 		return(
 		<div className="bannerLogin">
@@ -11,25 +58,37 @@ class Home extends React.Component{
 			 <title>Login</title>
 			</Helmet>
             <div className="loginDiv">
-                <form action='Home'>
+                    <form>
                     <h1>Welcome</h1>
 
                     <div className="col-3">
-                       <input className="effect-2" type="email" placeholder="Email"/>
+                            <input className="effect-2" type="email" placeholder="Email"
+                                value={
+                                    this.state.user.email
+                                }
+                                onChange={
+                                    (event) => this.updatePropertyValue(event.target.value, 'email')
+                                }/>
                     <span className="focus-border"></span>
                     </div>
                     <br></br>
                     <div className="col-3">
-                       <input className="effect-2" type="password" placeholder="Password"/>
+                            <input className="effect-2" type="password" placeholder="Password"
+                                value={this.state.user.password}
+                                onChange={
+                                    (event) => this.updatePropertyValue(event.target.value, 'password')
+                                }/>
                     <span className="focus-border"></span>
                     </div>
 
-                    <button type="submit" className="submitBtn">Login</button>
+                    <input type="button" onClick={this.login.bind(this)} className="loginButton" value="Login" />
                     
                 </form>
             </div>
 		</div>
 		);
-	}
+    }
+
+
 }
 export default Home;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,44 @@
-const express = require('express');
-const router = express.Router();
+var jwt = require('jsonwebtoken');
+var lib = process.cwd()
+var config = require(lib + '/config');
+var middleware = require(lib + '/middleware');
+
 
 //Login Page
-router.get('/login',  (req, res) => res.send('Login'));
+module.exports = function(app){
+app.post('/login',  function(req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    // For the given username fetch user from DB
+    let mockedUsername = 'admin@admin.com';
+    let mockedPassword = '123';
 
-module.exports = router;
+    if (username && password) {
+        if (username === mockedUsername && password === mockedPassword) {
+            let token = jwt.sign({username: username},
+                config.secret,
+                { expiresIn: '24h' // expires in 24 hours
+                }
+            );
+            // return the JWT token for the future API calls
+            res.json({
+                success: true,
+                message: 'Authentication successful!',
+                token: token
+            });
+        } else {
+            res.send(403).json({
+                success: false,
+                message: 'Incorrect username or password'
+            });
+        }
+    } else {
+        res.send(400).json({
+            success: false,
+            message: 'Authentication failed! Please check the request'
+        });
+    }
+});
+}
+
+//module.exports = router;
